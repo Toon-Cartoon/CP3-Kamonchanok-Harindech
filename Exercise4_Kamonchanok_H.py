5d3ef6a3e191c655d9b512f0b98353a00b1dc2d9
Toon <Toon@github.com>
2020-07-12 22:36:31 +0700

    Exercise 4

---
+++ Exercise4_Kamonchanok_H.py
@@ -0,0 +1,10 @@
+FE = 78
+GB = 81.5
+CS = 74.5
+CF = 70
+
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",CS)
+print("Computer Programming : ",CF)
